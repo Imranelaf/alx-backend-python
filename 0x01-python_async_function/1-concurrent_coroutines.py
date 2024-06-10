e192788f081d3d8489fd3a3029e0827f05ba0890
imrane ALI LAFKIH <imrane.laf@gmail.com>
2024-06-10 15:57:50 -0700

    Measure the runtime

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+This module contains the function wait_n(n: int, max_delay: int) that takes in
+two arguments and returns a list of delays in ascending order.
+'''
+
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*tasks))
