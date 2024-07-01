b9cc0a87325eed30da0fc1850b7373ed12ea8eed
imrane ALI LAFKIH <imrane.laf@gmail.com>
2024-07-01 12:51:43 -0700

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,3 +38,56 @@ class TestAccessNestedMap(unittest.TestCase):
         '''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """doc doc doc"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("requests.get")
+    def test_get_json(
+        self, test_url: str, test_payload: Dict[str, Any], mock_get: Mock
+    ) -> None:
+        '''
+        This method is for testing get_json
+        '''
+        mock_get.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    '''
+    This class is for testing memoize
+    '''
+    def test_memoize(self) -> None:
+        '''
+        This method is for testing memoize
+        '''
+        class TestClass:
+            '''
+            This class is for testing memoize
+            '''
+            def a_method(self) -> int:
+                '''
+                This method is for testing memoize
+                '''
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                '''
+                This method is for testing memoize
+                '''
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mocked:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mocked.assert_called_once()
